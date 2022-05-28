9c91211ab561359990d9b8a00803519db79d14d8
silent10z <dkgueing@gmail.com>
2022-05-28 16:29:34 +0900

    feat:디테일 페이지 시작

--- src/routes/ManageDetailPage/index.tsx
+++ src/routes/ManageDetailPage/index.tsx
@@ -8,7 +8,7 @@ const ManageDetailPage = () => {
       <nav className={styles.subNav}>홈 &gt; 회원관리 &gt; 회원상세</nav>
       <h1 className={styles.title}> 회원 상세 정보</h1>
 
-      <div className={styles.manageDetailContainer}>하위</div>
+      <div className={styles.manageDetailContainer}>시작한다@</div>
     </>
   )
 }

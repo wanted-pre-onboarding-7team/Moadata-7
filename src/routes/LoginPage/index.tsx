f33883bc8d2400fbf25950dcf2b7b2fcfce6af30
jiiiooiiij <88502596+HANJISUN@users.noreply.github.com>
2022-05-29 01:55:29 +0900

    feat: 로그인 팝업 구현

--- src/routes/LoginPage/index.tsx
+++ src/routes/LoginPage/index.tsx
@@ -1,10 +1,25 @@
-import SEO from 'components/SEO'
+import { useState } from 'react'
 import styles from './loginPage.module.scss'
 
+import SEO from 'components/SEO'
+import Button from 'components/Button'
+import PopupPortal from './Popup/PopupPortal'
+import Popup from './Popup'
+
 const LoginPage = () => {
+  const [isOpenPopup, setIsOpenPopup] = useState(false)
+
+  const handlebuttonClick = () => {
+    setIsOpenPopup(true)
+  }
+
   return (
     <div>
       <SEO title='로그인' />
+      <Button size='xsmall' onClick={handlebuttonClick} secondary>
+        modal open
+      </Button>
+      <PopupPortal>{isOpenPopup && <Popup setIsOpenPopup={setIsOpenPopup} />}</PopupPortal>
     </div>
   )
 }

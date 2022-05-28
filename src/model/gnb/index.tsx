a63051fd4904402ffe330595635edea7dcd7280e
silent10z <dkgueing@gmail.com>
2022-05-28 20:52:20 +0900

    feat: model 파일 생성 및 GNB_LIST 적용

---
+++ src/model/gnb/index.tsx
@@ -0,0 +1,14 @@
+const GNB_LIST = [
+  {
+    title: '백오피스 홈',
+    path: '',
+    key: 'back_1',
+  },
+  {
+    title: '회원관리',
+    path: 'member/manage',
+    key: 'back_1',
+  },
+]
+
+export { GNB_LIST }

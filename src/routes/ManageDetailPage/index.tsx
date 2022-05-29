ab8da0a5d140edde0282c0c6a7254e928bba9569
Hyohyun Nam <skagygus8@gmail.com>
2022-05-29 18:25:33 +0900

    feat:걸음수차트 기본구현

--- src/routes/ManageDetailPage/index.tsx
+++ src/routes/ManageDetailPage/index.tsx
@@ -1,5 +1,6 @@
 import SEO from 'components/SEO'
 import styles from './manageDetailPage.module.scss'
+import StepChart from './StepChart'
 
 const ManageDetailPage = () => {
   return (
@@ -7,8 +8,9 @@ const ManageDetailPage = () => {
       <SEO title='회원상세' />
       <nav className={styles.subNav}>홈 &gt; 회원관리 &gt; 회원상세</nav>
       <h1 className={styles.title}> 회원 상세 정보</h1>
-
       <div className={styles.manageDetailContainer}>하위</div>
+
+      <StepChart />
     </>
   )
 }

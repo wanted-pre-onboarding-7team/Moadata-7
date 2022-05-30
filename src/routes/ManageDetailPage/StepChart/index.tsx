7bf46bcdd4a66558e25fea939f3a635cad6debaf
Hyohyun Nam <skagygus8@gmail.com>
2022-05-30 16:12:13 +0900

    feat:날짜비교

--- src/routes/ManageDetailPage/StepChart/index.tsx
+++ src/routes/ManageDetailPage/StepChart/index.tsx
@@ -1,45 +1,57 @@
-import React from 'react'
-import { VictoryAxis, VictoryBar, VictoryChart, VictoryLabel, VictoryTheme, VictoryTooltip } from 'victory'
+import React, { useState, MouseEvent, useEffect, useMemo } from 'react'
 
-import dayjs from 'dayjs'
-import stepDataList from 'assets/data/step_data/step_136_0226_1.json'
+import { StepDB } from '../DB'
 import styles from './stepChart.module.scss'
+import dayjs from 'dayjs'
+import isSameOrBefore from 'dayjs/plugin/isSameOrBefore'
 
-const dailyStepData = stepDataList.reverse().map((item, idx) => {
-  return {
-    x: item.crt_ymdt,
-    y: item.steps,
-    index: idx,
-    label: item.steps,
-  }
-})
+import { userInfoState } from '../state'
+import { useRecoilValue } from 'recoil'
+import Chart from './Chart'
+import RangeDate from './RangeDate'
+
+dayjs.extend(isSameOrBefore)
 
 const StepChart = () => {
+  const userInfo = useRecoilValue(userInfoState)
+  const startDay = dayjs(userInfo.crt_ymdt).format('YYYY-MM-DD')
+  const [endDay, setEndDay] = useState(dayjs().format('YYYY-MM-DD'))
+
+  if (!userInfo.crt_ymdt) return null
+
+  const onClick = (e: MouseEvent<HTMLInputElement>) => {
+    if (!e.currentTarget.textContent) return
+    const onClickText = e.currentTarget.textContent
+
+    const endDayArray = {
+      시작일: startDay,
+      일주일: dayjs(userInfo.crt_ymdt).add(1, 'week').format('YYYY-MM-DD'),
+      전체: dayjs().format('YYYY-MM-DD'),
+    }[onClickText]
+
+    if (!endDayArray) return
+
+    setEndDay(endDayArray)
+  }
+
+  const StepDataList = StepDB[`member${userInfo.member_seq}`]
+    .filter((item) => {
+      return dayjs(item.crt_ymdt.split(' ')[0]).isSameOrBefore(dayjs(endDay))
+    })
+    .sort((a, b) => new Date(a.crt_ymdt).getTime() - new Date(b.crt_ymdt).getTime())
+    .map((item) => {
+      return {
+        x: item.crt_ymdt,
+        y: item.steps,
+        label: item.steps,
+      }
+    })
+
   return (
     <div className={styles.chartWrap}>
-      <VictoryChart theme={VictoryTheme.material} width={450}>
-        <VictoryAxis
-          dependentAxis
-          offsetX={460}
-          tickFormat={(x) => {
-            return x.toLocaleString()
-          }}
-          tickCount={7}
-          style={{
-            axis: { stroke: 'transparent' },
-            tickLabels: { fill: '#94A2AD' },
-          }}
-          tickLabelComponent={<VictoryLabel dy={20} />}
-        />
-        <VictoryAxis tickCount={2} />
-        <VictoryBar
-          style={{
-            data: { fill: ({ datum }) => (datum.index === dailyStepData.length - 1 ? '#C5C5CC' : '#F84B0B') },
-          }}
-          labelComponent={<VictoryTooltip />}
-          data={dailyStepData}
-        />
-      </VictoryChart>
+      <Chart StepDataList={StepDataList} />
+
+      <RangeDate onClick={onClick} startDay={startDay} endDay={endDay} />
     </div>
   )
 }
